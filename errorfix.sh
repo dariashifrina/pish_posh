d4a6b6afc3806862a3e3efd2e7108e506903b705
root <hhiggins@stuy.edu>
2018-06-04 03:52:20 +0000

    fixed errorfix.sh

--- errorfix.sh
+++ errorfix.sh
@@ -1,7 +1,7 @@
 sudo chmod 774 pish_posh
-sudo chgrp www-data pish-posh
-sudo chmod 774 pishposh/pish.db
-sudo chgrp www-data pish-posh/pish.db
-DBPATH=$(pwd)/pish.db
-sudo sed -i -e 's/pish.db/$DBPATH/g'
+sudo chgrp www-data pish_posh
+sudo chmod 774 pish_posh/pish.db
+sudo chgrp www-data pish_posh/pish.db
+DBPATH=$(pwd)/pish_posh/pish.db
+sudo sed -i -e "s|pish.db|$DBPATH|g" "pish_posh/db_stuff.py"
 sudo service apache2 reload
